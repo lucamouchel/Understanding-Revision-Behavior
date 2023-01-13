5b51c87fd3698ef474dd54839c5992b7eb7c9297
lucamouchel <luca.mouchel@epfl.ch>
2023-01-13 13:34:34 +0100

    converted PatMining and ProMining notebooks into py files and added to  utils.py

---
+++ processmining.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+import os
+import pm4py
+from pm4py.visualization.petri_net import  visualizer as pn_vis_factory
+from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py.algo.discovery.alpha import  algorithm as alpha_miner
+from utils import *
+
+def save_directly_follow_graph(csv_filepath, group_num):
+    dataframe = pd.read_csv(csv_filepath, sep=';')
+    event_log = pm4py.format_dataframe(dataframe, case_id='case_id', activity_key='activity', timestamp_key='timestamp')
+    event_log = pm4py.convert_to_event_log(dataframe)
+    performance_dfg, start_activities, end_activities = pm4py.discover_performance_dfg(event_log)
+    try:
+        pm4py.save_vis_performance_dfg(performance_dfg, start_activities, end_activities, 'results/process mining/groupnum3{n}.png'.format(n=group_num))
+    except: 
+        print('user only has 1 recipe')
+
+ACTIONS = [';1st recipe submitted;', ';revision;', ';second recipe submitted;']
+def format_user_data(user_index, case_id=1):
+    recipe_indices = map_[user_index].copy()
+    if user_index == len(sorted_users) - 1: last_index_where_written = len(df)
+    else: last_index_where_written = indices_of_first_attempts_per_user[user_index + 1]
+    recipe_indices.append(last_index_where_written)
+
+    where_in_df = np.where(df['user_id'] == sorted_users[user_index])
+
+    first_line = str(case_id) + ACTIONS[0] + df.iloc[where_in_df[0][0]]['event_date']             
+    lines = [first_line]
+    for i, index in enumerate(range(recipe_indices[0]+1, recipe_indices[-1]+1)):
+        j = index
+        is_new_recipe = index in recipe_indices
+        while not is_new_recipe:
+            line = ACTIONS[1] + df.iloc[where_in_df[0][i+1]]['event_date']
+            lines.append(line)
+            j+=1
+            if j in recipe_indices: is_new_recipe = True
+        try:
+            if index in recipe_indices : lines.append(ACTIONS[2] + df.iloc[where_in_df[0][i+1]]['event_date'])
+        except: continue
+        
+    result = lines
+    for i, line in enumerate(lines):
+        if line.startswith(ACTIONS[1]):
+            result[i] = str(case_id) + line
+           
+        elif line.startswith(ACTIONS[2]):
+            second_recipe_already_submitted = any([ACTIONS[2] in l for l in result[:i]])
+            if second_recipe_already_submitted:
+                result[i] = str(case_id) + line.replace('second', 'third') 
+            else : result[i] = str(case_id) + line
+        
+    for i, line in enumerate(result):
+        replaced = ACTIONS[2].replace('second', 'third')
+        if line.startswith(str(case_id) + replaced):
+            has_submitted_third_recipe = any([replaced in l for l in result[:i]])
+            if has_submitted_third_recipe:
+                result[i] =";;"
+            else : result[i] = line 
+            
+    return  pd.DataFrame(sorted(set(result), key=result.index), columns=['case_id;activity;timestamp'])
+
+for group in [1,2]:
+    dframes = []
+    for j, user in enumerate(INDICES[group-1]):
+        A = format_user_data(user_index=user, case_id=j+1)
+        dframes.append(A)
+
+    res = pd.concat(dframes)
+    path = 'data/processMiningData/group{i}.csv'.format(i=group)
+    res.to_csv(path, index=False)
+    save_directly_follow_graph(csv_filepath=path, group_num=group)
--- utils.py
+++ utils.py
@@ -1,4 +1,52 @@
 from datetime import datetime
+import nltk.corpus 
+from nltk.tokenize import word_tokenize, sent_tokenize
+import heapq
+import pandas as pd
+import gensim.downloader as api
+from scipy import spatial
+import numpy as np
+import pandas as pd
+import ast
+import numpy as np
+
+df = pd.read_csv('./data/keystrokes-recipes-modified.csv')
+indices_of_first_attempts_per_user = df.groupby('user_id').head(1).index
+noisy_punct = [',', '.', '-', ':', '(', ')']
+all_keystrokes = list(map(lambda _ : ast.literal_eval(_) , df['ks'].values))
+KEYWORDS = ['Alt', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'Backspace', 'CapsLock', 'Control', 'Delete', 'End', 'Enter', 'Home', 'Meta', 'PageUp', 'PageDown', 'PrintScreen','Shift', 'Tab']
+sorted_users = sorted(df['user_id'].unique())
+matching_data = pd.read_csv('data/groupmatching.csv')
+users_to_groups = dict(pd.read_csv('data/groupmatching.csv').filter(['user_id', 'group']).values)
+s = matching_data.sort_values(by=['group', 'user_id'], ascending=True)
+INDICES = []
+
+"""
+    Basically what we do here is simple. We have the data matching each user to which group they belong to.
+    So what we do is we create an array INDICES. We create a dictionnary which maps each group to the users in it.
+    Then iterate over the users of each group and find their index in the sorted users array. 
+
+    so INDICES has 2 arrays, each containing the indices of the users in that group.
+    
+"""
+groups = {1: [], 2: []}
+for i, dic in enumerate(s.values):
+    user = dic[1]
+    if user in sorted_users:
+        group = users_to_groups[user]
+        if group == 2:
+            groups[1].append(user)
+        elif group == 4:
+            groups[2].append(user)
+
+
+for group in [1,2]:
+    indices = []
+    for user in groups[group]:
+        i = np.where(np.array(sorted_users) == user)
+        indices.append(i[0][0])
+    INDICES.append(indices)
+
 
 def get_last_index_where_written(user_index, indices_of_first_attempts_per_user, num_users, num_entries):
     """
@@ -102,3 +150,142 @@ def compute_text(metric, data):
     group2=compute_diff_percentage(data.loc['group 2'])
     )
     return text
+
+def flatten(list):
+    """
+    flattens list of lists
+
+    Args:
+        list (list): list to flatten
+
+    Returns:
+        list: flattened list
+    """
+    return [item for sublist in list for item in sublist]
+
+##############
+def summarize(text):
+    """
+    returns a summary of 'text'
+
+    Args:
+        text (str): text to summarize
+
+    Returns:
+        str: summary of text
+    """
+    stopwords = nltk.corpus.stopwords.words('english')
+    word_frequencies = {}
+    try:
+        sentence_list = nltk.sent_tokenize(text)
+    except: 
+        print()
+    for word in nltk.word_tokenize(text):
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+    maximum_frequncy = max(word_frequencies.values())
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word]
+
+    summary_sentences = heapq.nlargest(5, sentence_scores, key=sentence_scores.get)
+    summary = ' '.join(summary_sentences)
+    return summary
+
+
+###########################
+def separate_sessions():
+    """
+    Separates writing sessions by identifying all indices in which new recipes are written
+    by calculating the cosine distance between the current recipe and the previous recipe and 
+    if the distance is greater than a threshold, then it is a new recipe
+
+    Returns:
+        list: indices of new recipes
+    """
+    model = api.load("glove-wiki-gigaword-50")
+    def preprocess(s):
+        res = ""
+        for i, char in enumerate(list(s)):
+            if char not in noisy_punct:
+                res += char    
+        res = [i.lower() for i in res.split()]
+        res = list(filter(lambda _ : _ not in noisy_punct, res))
+        return res
+
+    def get_vector(s):
+        """
+        Get the vector representation of a sentence from the model
+
+        Args:
+            s (str): text
+
+        """
+        arr = []
+        for i in preprocess(s):
+            key = None
+            try: 
+                key = model[i]
+                arr.append(key)
+            except:
+                continue
+
+        arr = np.array(arr)
+        return np.sum(arr, axis=0)
+
+    recipes = df['recipe'].values
+
+    def compute_recipe_indices(start_index, acc):
+        """
+        Computes the list of indices where each recipe in the dataset begins
+        Basically, user 0 writes 3 recipes:
+        starts writing at t = 0, revises once at t = 1, a second time at t = 2 and 
+        starts a new recipe at t = 3, then this function will return [0, 3] 
+
+        Args:
+            start_index (int): index to compare with the other recipes
+            acc (list(int)): list to return
+
+        Returns:
+            list(int) : list of indices of beginning of each recipe
+        """
+        if start_index >= len(recipes) - 1:
+            return acc
+        vec = get_vector(recipes[start_index])
+        for i in range(start_index, len(recipes)):
+            dist = 1 - spatial.distance.cosine(vec, get_vector(recipes[i]))
+            if dist < .995:
+                acc.append(i)
+                return compute_recipe_indices(i, acc)
+
+    recipes_indices = compute_recipe_indices(0, [0])
+
+    # Out of 450 samples, we only have  31 misclassified samples that are misclassified as new recipes so the algorithm is pretty effective
+    to_remove = [13, 116, 134, 156, 168, 188, 249, 255, 256, 403, 88, 90, 128, 209, 376, 379, 381, 390, 391, 393, 394,395,  444]
+    add = [121, 204, 254, 336, 97, 360, 362, 392]
+    for i in to_remove:
+        recipes_indices.remove(i)
+
+    for i in add:
+        recipes_indices.append(i) 
+
+    recipes_indices = sorted(recipes_indices)
+
+    rec = [df['recipe'][i] for i in recipes_indices]
+    users = [df['user_id'][i] for i in recipes_indices]
+    dframe = pd.DataFrame([recipes_indices, rec, users]).transpose()
+    dframe.columns =['recipe index in data', 'recipe', 'user id']
+    return dframe
+
+map_ = separate_sessions().groupby('user id')["recipe index in data"].apply(list)
